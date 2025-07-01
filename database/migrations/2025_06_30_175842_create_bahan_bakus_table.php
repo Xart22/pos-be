177f53b9dc0846e054deb55b5e33d1effd5ff9ce
fajar.rn22@gmail.com <fajar.rn22@gmail.com>
2025-07-02 01:09:14 +0700

    l

---
+++ database/migrations/2025_06_30_175842_create_bahan_bakus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bahan_bakus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')
+                ->constrained('category_bahan_bakus')
+                ->onDelete('cascade');
+            $table->bigInteger('harga');
+            $table->integer('stok')->default(0);
+            $table->string('satuan')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bahan_bakus');
+    }
+};
